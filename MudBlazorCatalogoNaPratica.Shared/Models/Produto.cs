617983958d211f2edd3ae07d4054d88f909b486f
João Pedro Binda da Silva <bindadasilvajoaopedro@gmail.com>
2026-04-18 11:11:37 -0300

    Validação e autocomplete em Produto; ajustes de UI
    Adicionadas DataAnnotations em Produto para validação de campos obrigatórios e restrições. Preço agora é exibido como moeda brasileira. Campo de categoria no formulário alterado para autocomplete com busca. Melhorias de usabilidade e acessibilidade. Atualização nos arquivos do banco de dados de categorias.

--- MudBlazorCatalogoNaPratica.Shared/Models/Produto.cs
+++ MudBlazorCatalogoNaPratica.Shared/Models/Produto.cs
@@ -8,15 +8,20 @@ namespace MudBlazorCatalogoNaPratica.Shared.Models
     public class Produto
     {
         public int ProdutoId { get; set; }
+        [Required(ErrorMessage = "O nome do produto é obrigatório.")]
         [MaxLength(100)]
         public string? Nome { get; set; }
+        [Required(ErrorMessage = "A descrição do produto é obrigatória.")]
         [MaxLength(200)]
         public string? Descricao { get; set; }
+        [Required(ErrorMessage = "O preço deve ser informado")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
         [Column(TypeName = "decimal(12,2)")]
         public decimal Preco { get; set; }
        
         public string? Imagem { get; set; }
-
+        
+        [Required(ErrorMessage = "A categoria do produto é obrigatória.")]
         public int CategoriaId { get; set; }
         public virtual Categoria? Categoria { get; set; }
     }
