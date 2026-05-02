40e763591bd6f94cd329bed23490cd9698cf73ec
João Pedro Binda da Silva <bindadasilvajoaopedro@gmail.com>
2026-05-02 14:30:24 -0300

    incluido pacote de autorização

---
+++ MudBlazorCatalogoNaPratica/MudBlazorCatalogoNaPratica.Client/Auth/AuthStateProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace MudBlazorCatalogoNaPratica.Client.Auth
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var usuario = new ClaimsIdentity();
+
+			return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(usuario)));
+        }
+    }
+}
--- MudBlazorCatalogoNaPratica/MudBlazorCatalogoNaPratica.Client/Program.cs
+++ MudBlazorCatalogoNaPratica/MudBlazorCatalogoNaPratica.Client/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using MudBlazorCatalogoNaPratica.Client.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient 
     { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
 await builder.Build().RunAsync();
